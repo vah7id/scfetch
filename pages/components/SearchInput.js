836fd445f7ff0b4cb2724e5a6a5223d2d827b013
Vahid Taghizadeh <vahid@Vahids-MacBook-Pro.local>
2023-02-02 18:23:15 +0100

    timeout for trim has been changed

--- pages/components/SearchInput.js
+++ pages/components/SearchInput.js
@@ -359,7 +359,7 @@ export default function SearchInput() {
                         anchorAudio.download = response.trimmedURL;
                         anchorAudio.click();
                         setIsFetching(false);
-                    }, 8000);
+                    }, 5000);
              
                 }).catch(err => {
                     showNotification('error', 'Oopss, It seems there was an issue with trimming the origin file! Please try a again!!');
